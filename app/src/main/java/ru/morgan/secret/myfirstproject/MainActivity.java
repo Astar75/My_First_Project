302ba4cbd442c87c043fa5f498dabf1ae47fed46
shigin.rv <astar.in.black@mail.ru>
2023-07-24 16:08:18 +0300

    Изучил как работают кнопки

--- app/src/main/java/ru/morgan/secret/myfirstproject/MainActivity.java
+++ app/src/main/java/ru/morgan/secret/myfirstproject/MainActivity.java
@@ -3,11 +3,25 @@ package ru.morgan.secret.myfirstproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import ru.morgan.secret.myfirstproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Привет мир", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
